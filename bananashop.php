3920b5df9ed7ed8fc2e665c92f7ace44f4cc2b16
msk1 <skarbala.martin@gmail.com>
2018-04-23 19:10:32 +0200

    feat added background updated design

--- bananashop.php
+++ bananashop.php
@@ -1,52 +1,94 @@
 <?php
 ?>
 <!doctype html>
-<body lang="en" xmlns:v-on="http://www.w3.org/1999/xhtml">
+<html lang="en">
 <?php include 'partials/head.php'; ?>
+<link href="https://fonts.googleapis.com/css?family=Secular+One" rel="stylesheet">
+<body>
 <?php include 'partials/navigation.php' ?>
-<div class="container">
+<style>
+    .navbar-default .navbar-nav > li > a, .navbar-default .navbar-brand {
+        font-family: 'Secular One', sans-serif;
+       color: #c07b1a;
+    }
+
+    .navbar-default .navbar-nav > .active > a,
+    .navbar-default .navbar-nav > .active > a:focus,
+    .navbar-default .navbar-nav > .active > a:hover,
+    .navbar-default .navbar-brand:focus, .navbar-default .navbar-brand:hover,
+    .navbar-default .navbar-nav > li > a:hover {
+        color: #ffc832;
+        background: none;
+    }
+body::after {
+    content: "";
+    background: url("../assets/img/banana_background.jpg");
+    opacity: 0.2;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    z-index: -1;
+}
+   h1 {
+        font-family: 'Secular One', sans-serif;
+        font-size: 70px;
+        font-weight: 600;
+        color: #ffc832 ;
+        text-shadow: 2px 2px #ec971f;
+    }
+    table tr:last-child{
+      font-weight: 700;
+      font-size: 17px;
+    }
+    table{
+      padding: 0px;
+      background-color: white;
+    }
+    button.btn, input.form-control{
+      border-radius: 0px;
+    }
+    button.btn{
+      margin-bottom: 20px;
+    }
+</style>
   <h1 class="text-center">Banana shop</h1>
+<div class="container">
   <div class="row">
-    <div class="col-md-6">
-      <label for="numberOfBananas">Number of bananas</label>
+    <div class="col-md-6 col-md-offset-3 main-content">
+      <!-- <label for="numberOfBananas">Number of bananas</label> -->
       <input v-model="numberOfBananas"
              type="number"
              class="form-control"
              v-on:keyup="clear"
+             placeholder="number of bananas"
       >
       <button v-on:click="calculatePrice"
-              class="btn btn-block btn-warning">
-        Calculate the price
-      </button>
-
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-6">
-      <table class="table table-bordered">
+              class="btn btn-block btn-warning">Give me the price!</button>
+      <table class="table table-hover">
         <tr>
           <td>Price for one banana</td>
-          <td>{{bananaPrice}}</td>
+          <td>{{bananaPrice}} €</td>
         </tr>
         <tr>
           <td>Number of bananas</td>
           <td>{{numberOfBananas}}</td>
         </tr>
         <tr>
-          <td>Total Price before dicount</td>
-          <td>{{fullPrice}}</td>
+          <td>Price before discount</td>
+          <td>{{fullPrice}} €</td>
         </tr>
         <tr>
           <td>Discount</td>
-          <td>{{totalDiscount}}</td>
+          <td>{{totalDiscount}} €</td>
         </tr>
-        <tr>
+        <tr class="warning">
           <td>Total Price</td>
-          <td>{{priceAfterDiscount}}</td>
+          <td>{{priceAfterDiscount}} €</td>
         </tr>
       </table>
     </div>
-  </div>
 </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/vue"></script>
@@ -54,27 +96,20 @@
     var app = new Vue({
         el: '.container',
         data: {
-            bananaPrice: 10,
+            bananaPrice: 1.5,
             priceAfterDiscount: 0,
-            numberOfBananas: 0,
+            numberOfBananas:0,
             totalDiscount: 0,
-            fullPrice: 0
+            fullPrice: 0,
+            discountRate:0,
         },
         methods: {
-            calculateDiscount: function () {
-                if (this.numberOfBananas > 10) {
-                    this.fullPrice * 0.2;
-                }
-                if (this.numberOfBananas > 20) {
-                    this.fullPrice * 0.4;
-                }
-            },
-
             calculatePrice: function () {
                 if (isNumeric(this.numberOfBananas)) {
                     this.fullPrice = this.numberOfBananas * this.bananaPrice;
-                    this.totalDiscount = this.calculateDiscount();
-                    this.priceAfterDiscount = this.fullPrice - this.calculateDiscount();
+                    this.priceAfterDiscount =
+                     Math.round(this.fullPrice * (1-calculateDiscountRate(this.numberOfBananas)),2);
+                    this.totalDiscount = this.fullPrice - this.priceAfterDiscount;
                 }
             },
 
@@ -90,5 +125,17 @@
     function isNumeric(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     }
+    function calculateDiscountRate(numberOfBananas){
+         switch(true) {
+    case (numberOfBananas>10 && numberOfBananas <20):
+        return 0.1;
+    case (numberOfBananas>=20):
+      return 0.2;
+       break;
+    default:
+     return 0;
+     }
+}
+
 </script>
 </html>
