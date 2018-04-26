e0fb2a968adb3160e1d0e53807c85190c42faa29
martin.skarbala <martin.skarbala@itera.no>
2018-04-26 13:12:58 +0200

    formatting update new font for table

--- bananashop.php
+++ bananashop.php
@@ -1,28 +1,30 @@
 <?php
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="en" xmlns:v-on="http://www.w3.org/1999/xhtml">
 <?php include 'partials/head.php'; ?>
 <link href="https://fonts.googleapis.com/css?family=Secular+One" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet">
 <body>
 <?php include 'partials/navigation.php' ?>
 <style>
-    .navbar-default .navbar-nav > li > a, .navbar-default .navbar-brand {
-        font-family: 'Secular One', sans-serif;
-       color: #c07b1a;
-    }
+  .navbar-default .navbar-nav > li > a, .navbar-default .navbar-brand {
+    font-family: 'Secular One', sans-serif;
+    color: #c07b1a;
+  }
 
-    .navbar-default .navbar-nav > .active > a,
-    .navbar-default .navbar-nav > .active > a:focus,
-    .navbar-default .navbar-nav > .active > a:hover,
-    .navbar-default .navbar-brand:focus, .navbar-default .navbar-brand:hover,
-    .navbar-default .navbar-nav > li > a:hover {
-        color: #ffc832;
-        background: none;
-    }
-body::after {
+  .navbar-default .navbar-nav > .active > a,
+  .navbar-default .navbar-nav > .active > a:focus,
+  .navbar-default .navbar-nav > .active > a:hover,
+  .navbar-default .navbar-brand:focus, .navbar-default .navbar-brand:hover,
+  .navbar-default .navbar-nav > li > a:hover {
+    color: #ffc832;
+    background: none;
+  }
+
+  body::after {
     content: "";
-    background: url("../assets/img/banana_background.jpg");
+    background: url("assets/img/banana_background.jpg");
     opacity: 0.2;
     top: 0;
     left: 0;
@@ -30,30 +32,43 @@ body::after {
     right: 0;
     position: absolute;
     z-index: -1;
-}
-   h1 {
-        font-family: 'Secular One', sans-serif;
-        font-size: 70px;
-        font-weight: 600;
-        color: #ffc832 ;
-        text-shadow: 2px 2px #ec971f;
-    }
-    table tr:last-child{
-      font-weight: 700;
-      font-size: 17px;
-    }
-    table{
-      padding: 0px;
-      background-color: white;
-    }
-    button.btn, input.form-control{
-      border-radius: 0px;
-    }
-    button.btn{
-      margin-bottom: 20px;
-    }
+  }
+
+  h1 {
+    font-family: 'Secular One', sans-serif;
+    font-size: 70px;
+    font-weight: 600;
+    color: #ffc832;
+    text-shadow: 2px 2px #ec971f;
+  }
+
+  table tr:last-child {
+    font-weight: 700;
+    font-size: 25px;
+  }
+
+  table {
+    padding: 0;
+    background-color: white;
+    font-family: 'Kalam', Arial, serif;
+    font-size: 20px;
+
+  }
+  table td{
+    color: #444444;
+  }
+
+  button.btn, input.form-control {
+    border-radius: 0;
+  }
+
+  button.btn {
+    font-family: 'Kalam', Arial, serif;
+    margin-bottom: 20px;
+    font-size: 20px;
+  }
 </style>
-  <h1 class="text-center">Banana shop</h1>
+<h1 class="text-center">Banana shop</h1>
 <div class="container">
   <div class="row">
     <div class="col-md-6 col-md-offset-3 main-content">
@@ -65,7 +80,8 @@ body::after {
              placeholder="number of bananas"
       >
       <button v-on:click="calculatePrice"
-              class="btn btn-block btn-warning">Give me the price!</button>
+              class="btn btn-block btn-warning">Give me the price!
+      </button>
       <table class="table table-hover">
         <tr>
           <td>Price for one banana</td>
@@ -89,7 +105,7 @@ body::after {
         </tr>
       </table>
     </div>
-</div>
+  </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 <script>
@@ -98,17 +114,17 @@ body::after {
         data: {
             bananaPrice: 1.5,
             priceAfterDiscount: 0,
-            numberOfBananas:0,
+            numberOfBananas: 0,
             totalDiscount: 0,
             fullPrice: 0,
-            discountRate:0,
+            discountRate: 0
         },
         methods: {
             calculatePrice: function () {
                 if (isNumeric(this.numberOfBananas)) {
                     this.fullPrice = this.numberOfBananas * this.bananaPrice;
                     this.priceAfterDiscount =
-                     Math.round(this.fullPrice * (1-calculateDiscountRate(this.numberOfBananas)),2);
+                        Math.round(this.fullPrice * (1 - calculateDiscountRate(this.numberOfBananas)));
                     this.totalDiscount = this.fullPrice - this.priceAfterDiscount;
                 }
             },
@@ -122,20 +138,20 @@ body::after {
         }
     });
 
-    function isNumeric(n) {
-        return !isNaN(parseFloat(n)) && isFinite(n);
+      function isNumeric(n) {
+          return !isNaN(parseFloat(n)) && isFinite(n);
+      }
+
+    function calculateDiscountRate(numberOfBananas) {
+        switch (true) {
+            case (numberOfBananas > 10 && numberOfBananas < 20):
+                return 0.1;
+            case (numberOfBananas >= 20):
+                return 0.2;
+            default:
+                return 0;
+        }
     }
-    function calculateDiscountRate(numberOfBananas){
-         switch(true) {
-    case (numberOfBananas>10 && numberOfBananas <20):
-        return 0.1;
-    case (numberOfBananas>=20):
-      return 0.2;
-       break;
-    default:
-     return 0;
-     }
-}
 
 </script>
 </html>
